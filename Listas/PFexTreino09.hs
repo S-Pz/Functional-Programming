81a9f2e85bb9ecf94f60e219b1b6e1f4b6324237
S-Pz <savio_fr@outlook.com>
2025-06-17 10:46:21 -0300

    Adding PFexTreino06.hs and Trabalho_2

---
+++ Listas/PFexTreino09.hs
@@ -0,0 +1,18 @@
+
+Sempre defina o cabeçalho das funções. Use a notação polimórfica se necessário. 
+Sempre use definições locais casos seja necessário criar funções auxiliares que são usadas por uma única função
+e para calcular valores repetidos ou que envolvam mais de uma operação. 
+
+Os tipos e funções das 4 primeiras questões devem ser feitas em um módulo chamado Aluno. As funções das 
+Questões 5, 6 e 7 deve ficam no módulo Main. 
+
+
+1. Crie o tipo algébrico Aluno com registro (Int) e nome (String).
+Defina que o tipo Alunopertencea classe Show formando uma String no formato Aluno Registro - Nome. Omesmoparaaclasse Read. 
+
+2. Crie uma função que recebe o nome de um arquivo e um aluno e grava seus dadosemumanova linha ao final do arquivo, mantendo os alunos gravados anteriormente. 
+3. Crie uma função que retorna uma lista como todos os Alunos de umarquivo cujonomeépassado como entrada dessa função. 
+4. Crie uma função recebe o nome de um arquivo e de um aluno e retorna MaybeAluno, quecontém o aluno gravado no arquivo que possui o nome passado, ou Nothing, casonenhumaluno com o nome passado seja encontrado no arquivo. 
+5. Crie uma função que mostra um menu para o usuário escolher entre sair doprograma,cadastrar um aluno, buscar um aluno ou visualizar todos os alunos. Essa funçãoretornaaescolha do usuário. 
+6. Crie uma função que apresente o menu da Questão 5 e permita ao usuário realizar asaçõesdefinidas nesse menu até que ele escolha sair do programa. Use o que você fez nasquestõesanteriores. 
+7. Crie uma função main que usa a função da Questão 6.
