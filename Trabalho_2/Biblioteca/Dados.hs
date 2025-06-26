78874329e57284f52d3c107fa92c39b1877bb9fd
Luiz Filipe Magalhães Freitas <felipe22331@gmail.com>
2025-06-26 02:34:01 -0300

    started Q2

--- Trabalho_2/Biblioteca/Dados.hs
+++ Trabalho_2/Biblioteca/Dados.hs
@@ -1,5 +1,25 @@
 module Biblioteca.Dados where
 
-    class Dados d where
-        toString :: d -> String
-        size :: d -> Int
+class Dados d where
+    toString :: d -> String
+    size :: d -> Int
+
+insert :: a -> Int -> Set a -> Set a          -- Insere um item na lista
+--remove :: a -> Set a -> Set a                    -- Remove um item na lista
+--search :: a -> Set a -> a                        -- Busca um item na lista
+--setEmpty :: Set a -> Bool                        -- Verifica se a fila está vazia
+
+data Set t = EmptySet | St t (Set t)
+
+instance (Show t) => Show (Set t) where
+  show ls = "[" ++ (impr ls) ++ "]" 
+
+impr :: Show t => Set t -> String
+impr (EmptySet) = ""
+impr (St x EmptySet) = show x
+impr (St x ls) = show x ++ "," ++ impr ls
+
+insert x 0 (EmptySet) = St x EmptySet
+insert _ _ (EmptySet) = error "Posição inválida"
+insert x 0 (St y ls) = St x (St y ls)
+insert x i (St y ls) = St y (insert x (i-1) ls)
--- Trabalho_2/Biblioteca/Main.hs
+++ Trabalho_2/Biblioteca/Main.hs
@@ -1,8 +1,12 @@
 module Main where
 import Biblioteca.Util
+import Biblioteca.Dados
 
 main :: IO()
 main = do
     putStrLn "Main..."
-    let data1 = Data { dia = 5, mes = 3, ano = 2025 }
-    putStrLn (dataStr data1)
+    
+    let lista2 = insert 10 0 EmptySet
+    putStrLn ("Lista após adição: " ++ show lista2)
+    let lista3 = insert 100 1 lista2
+    putStrLn ("Lista após segunda adição: " ++ show lista3)
